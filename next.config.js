8cf0ba694a5bd4db53b62f3c61d606fe30790dc0
DKillia <kunju.yadav555@gmail.com>
2023-10-06 21:29:40 +0530

    7th commit

--- next.config.js
+++ next.config.js
@@ -1,4 +1,12 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+    images: {
+        domains: [
+            'scontent.fbir1-1.fna.fbcdn.net',
+            'beforeigosolutions.com',
+            'i.ibb.co'
+        ],
+    },
+}
 
 module.exports = nextConfig
