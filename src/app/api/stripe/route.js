faf39fd78bd702143dc57f8882d5dd10608f880f
DKillia <kunju.yadav555@gmail.com>
2023-10-06 20:47:49 +0530

    6th commit

--- src/app/api/stripe/route.js
+++ src/app/api/stripe/route.js
@@ -17,8 +17,8 @@ export async function POST(req) {
         payment_method_types: ["card"],
         line_items: res,
         mode: "payment",
-        success_url: "http://localhost:3000/checkout" + "?status=success",
-        cancel_url: "http://localhost:3000/checkout" + "?status=cancel",
+        success_url: "https://gleaming-melomakarona-708f50.netlify.app/checkout" + "?status=success",
+        cancel_url: "https://gleaming-melomakarona-708f50.netlify.app/checkout" + "?status=cancel",
       });
 
       return NextResponse.json({
